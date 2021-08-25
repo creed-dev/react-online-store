62366883906c298946ca1e286efcdbfab6f189b0
Creed <creed-dev@internet.ru>
2021-08-25 19:39:56 +0700

    upd Header and SortPopup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Redirect, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import ProductsAirPods from './components/Products/ProductsAirPods/ProductsAirPods';
-import ProductsAll from './components/Products/ProductsAll/ProductsAll';
 import ProductsIpad from './components/Products/ProductsIpad/ProductsIpad';
 import ProductsIphone from './components/Products/ProductsIphone/ProductsIphone';
 import ProductsMac from './components/Products/ProductsMac/ProductsMac';
@@ -17,7 +16,6 @@ const App = props => {
 			<div className="container">
 				<Navbar />
 				<Switch>
-					<Route path="/all" component={ProductsAll}></Route>
 					<Route path="/iphone" component={ProductsIphone}></Route>
 					<Route path="/ipad" component={ProductsIpad}></Route>
 					<Route path="/mac" component={ProductsMac}></Route>
